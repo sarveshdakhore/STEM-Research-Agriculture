92bd262b730fa9b36e1aa941048fb1aa01e3de30
sarveshdakhore <72350223+sarveshdakhore@users.noreply.github.com>
2022-03-31 23:53:39 +0530

    Update plot.py

--- STEM Code and Data/plot.py
+++ STEM Code and Data/plot.py
@@ -1,5 +1,17 @@
 import matplotlib.pyplot as plt
-import pandas as pd
+import pandas as pd 
+
+while True:
+  try:
+    dpi = int(input("\n\nDPI(max-3000, min-100): "))
+    if dpi<0:
+      dpi = dpi*-1
+    if dpi>3000 or dpi<100:
+      print("Out of limit\n")
+    else:
+      break
+  except:
+    print("Unexpected Input... Try Again\n\n")
 
 def clearAllPlots():
   plt.figure().clear()
@@ -10,7 +22,16 @@ def clearAllPlots():
 DataFrame = pd.read_csv("dataset.csv")
 variableId = (DataFrame["Variable Id"].unique()) 
 
+def percentage(current,total):
+  percent = (current*100/total)
+  print(f"{percent:.1f}", end='\r')
+  if int(percent) == 100:
+    print("Task Completed")
+  
+current = 0
+total = int(len(variableId))
 for varId in variableId:
+  current+=1
   filteredDataViaVariableId = DataFrame[(DataFrame["Variable Id"]==int(varId))]
   contries = (filteredDataViaVariableId["Area"].unique())
   for country in contries:
@@ -26,13 +47,12 @@ for varId in variableId:
     plt.plot(x1, y1, label = country)
     
   title=list(DataFrame.loc[(DataFrame["Variable Id"]==int(varId)),"Variable Name"])[0]
-  plt.title(title) #title plotting
+  plt.title(title)
   
   # show a legend on the plot
   plt.legend()
   
   # function to save the plot
-  plt.savefig("./Graphs/"+str(varId),dpi=2000)
+  plt.savefig("./Graphs/"+str(varId),dpi=dpi)
   clearAllPlots()
-  
-  
+  percentage(current=current,total=total)
